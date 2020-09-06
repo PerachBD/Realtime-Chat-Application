6ec6313f877c45a87b3a22abc0c96750096c3e57
Perach Ben David <perach13@gmail.com>
2020-09-06 17:38:00 +0300

    ui chat window changes

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import './Chat.css';
+
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 let socket;
+
 
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
@@ -45,10 +50,8 @@ const Chat = ({ location }) => {
     return (
         <div className="outerContainer">
             <div className="container">
-                <input 
-                value={message} 
-                onChange={(event) => setMessage(event.target.value)}
-                onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null }/>
+                <InfoBar room={room} />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
         </div>
     )
