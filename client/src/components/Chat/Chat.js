a68cf5663e7807e827ccae3cab2007e943c34364
Perach Ben David <perach13@gmail.com>
2020-09-07 13:32:10 +0300

    show connected users

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,13 +9,14 @@ import Input from '../Input/Input';
 import Messages from '../Messages/Messages';
 let socket;
 
-
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = `https://react-chat-application5.herokuapp.com/`;
+    const [users, setUsers] = useState([]);
+    const ENDPOINT = `http://localhost:5000`;
+    console.log(ENDPOINT);
 
     useEffect( () => {
         const { name, room } = queryString.parse(location.search);
@@ -37,7 +38,16 @@ const Chat = ({ location }) => {
         socket.on('message', (message) => {
             setMessages([...messages, message]);
         })
+        socket.on('roomData', (roomData) => {
+            // setUsers(...users);
+            setUsers(roomData.users.map(element => element.name));
+            console.log(users)
+        })
     }, [messages]);
+
+    useEffect(()=>{
+        
+    },[users])
 
     const sendMessage = (event) => {
         event.preventDefault();
@@ -47,14 +57,17 @@ const Chat = ({ location }) => {
     }
 
     console.log(message, messages);
-
+    const userlist = users.map((item)=>(<p>{item},</p>));
+    console.log(userlist)
     return (
         <div className="outerContainer">
             <div className="container">
-                <InfoBar room={room} />
+                <InfoBar room={room} users={users} />
+                {/* <div>connected: {userlist}</div> */}
                 <Messages messages={messages} name={name}/>
                 <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
+            
             {/* <TextContainer users={users}/> */}
         </div>
     )
--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -4,14 +4,21 @@ import './InfoBar.css';
 import closeIcon from '../../icons/closeIcon.png';
 import onlineIcon from '../../icons/onlineIcon.png';
 
-const InfoBar = ({ room }) => (
-    <div className="infoBar">
-        <div className="leftInnerContainer">
-            <img className="onlineIcon" src={onlineIcon} alt="online" />
-            <h3>{room}</h3>
+const InfoBar = ({ room, users }) => (
+    <div>
+        <div className="infoBar">
+            <div className="leftInnerContainer">
+                <img className="onlineIcon" src={onlineIcon} alt="online" />
+                <h3>{room}</h3>
+            </div>
+            
+            <div className="RightInnerContainer">
+                <a href="/"><img src={closeIcon} alt="close" /></a>
+            </div>
+            
         </div>
-        <div className="RightInnerContainer">
-            <a href="/"><img src={closeIcon} alt="close" /></a>
+        <div>
+            connected:{users.map((item,index)=>(<p style={{display:"inline"}} key={index}> {item}, </p>))}
         </div>
     </div>
 )
